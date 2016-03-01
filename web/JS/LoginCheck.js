276c58bbc20771766ff56087965322c991ba1baa
limitwh <limitwh@163.com>
2016-03-01 23:15:30 +0800

    JS change

--- web/JS/LoginCheck.js
+++ web/JS/LoginCheck.js
@@ -2,29 +2,68 @@
  * Created by mliu3 on 2016/2/26.
  */
 function checkUserName(userName){
+    if(userName.length === 0){
+        document.getElementById("username").focus();
+        document.getElementById("UserNameErrMSG").innerHTML="<font color='red' size='2'>User Name not empty!</font>";
+        return false;
+    }else{
+        document.getElementById("UserNameErrMSG").innerHTML="";
+    }
     if(userName.length<3||userName.length>10){
         document.getElementById("username").focus();
         document.getElementById("UserNameErrMSG").innerHTML="<font color='red' size='2'>User name between 3-10 byte!</font>";
         return false;
+    }else{
+        document.getElementById("UserNameErrMSG").innerHTML="";
     }
-    if(isSpace(userName)){
+    if(inSpace(userName)){
         document.getElementById("username").focus();
         document.getElementById("UserNameErrMSG").innerHTML="<font color='red' size='2'>No space in user name!</font>";
         return false;
+    }else{
+        document.getElementById("UserNameErrMSG").innerHTML="";
     }
-    if(isNumOrChar(userName)){
-        //form.username.onfocus();
+    if(!isNumOrChar(userName)){
+        document.getElementById("username").focus();
         document.getElementById("UserNameErrMSG").innerHTML="<font color='red' size='2'>User name must use number or char!</font>";
         return false;
+    }else{
+        document.getElementById("UserNameErrMSG").innerHTML="";
     }
-    //document.getElementById("UserNameErrMSG").innerHTML="<font color='red' size='2'>User Name not empty!</font>";
 }
 
-function checkPSWName(){
-    //document.getElementById("UserPSWErrMSG").innerHTML="<font color='red'>Password not empty!</font>";
+function checkPSW(PSW){
+    if(PSW.length === 0){
+        document.getElementById("psw").focus();
+        document.getElementById("UserPSWErrMSG").innerHTML="<font color='red' size='2'>Password not empty!</font>";
+        return false;
+    }else{
+        document.getElementById("UserPSWErrMSG").innerHTML="";
+    }
+    if(PSW.length<3||PSW.length>10){
+        document.getElementById("psw").focus();
+        document.getElementById("UserPSWErrMSG").innerHTML="<font color='red' size='2'>Password between 3-10 byte!</font>";
+        return false;
+    }else{
+        document.getElementById("UserPSWErrMSG").innerHTML="";
+    }
+    if(inSpace(PSW)){
+        document.getElementById("psw").focus();
+        document.getElementById("UserPSWErrMSG").innerHTML="<font color='red' size='2'>No space in Password!</font>";
+        return false;
+    }else{
+        document.getElementById("UserPSWErrMSG").innerHTML="";
+    }
+    if(!isNumOrChar(PSW)){
+        document.getElementById("psw").focus();
+        document.getElementById("UserPSWErrMSG").innerHTML="<font color='red' size='2'>Password must use number or char!</font>";
+        return false;
+    }else{
+        document.getElementById("UserPSWErrMSG").innerHTML="";
+    }
 }
 
-function isSpace(string){
+function inSpace(string){
     var spacechar=' \t\n\r';
     var i,c;
     for(i=0;i< string.length;i++){
@@ -42,5 +81,13 @@ function isNumOrChar(str){
         return true;
     }else{
         return false;
+    }
+}
+
+function submitCheck(){
+    if((document.getElementById("UserNameErrMSG").innerHTML.length === 0)&&(document.getElementById("UserPSWErrMSG").innerHTML.length === 0)){
+        alert("all go!");
+    }else{
+        alert("error!");
     }
 }
